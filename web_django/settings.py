4c0fcad66f16c17a24d890da51d173e90a96e8c3
KitsuneSensei810 <sanyavoytenko_2001@mail.ru>
2023-05-11 11:44:26 +1000

    123

--- web_django/settings.py
+++ web_django/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'composik',
         'USER': 'admin',
         'PASSWORD': 'admin',
-        'HOST': 'db',
+        'HOST': '10.0.2.15',
         'PORT': '3306',
     }
 }

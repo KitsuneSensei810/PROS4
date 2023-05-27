060951bff4f19bd20b1eb300947fcb34b925e53c
KitsuneSensei810 <sanyavoytenko_2001@mail.ru>
2023-05-27 15:48:30 +1000

    123

--- main/forms.py
+++ main/forms.py
@@ -5,7 +5,7 @@ from django.forms import EmailInput, ModelForm, TextInput
 class MainForm(ModelForm):
     class Meta:
         model = main
-        fields = ["first_name", "second_name", "phonenumber", "email"]
+        fields = ["first_name", "second_name", "phonenumber", "email", "test_name"]
         widgets = {
 
             "first_name": TextInput(attrs={
@@ -29,6 +29,12 @@ class MainForm(ModelForm):
             "phonenumber": TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Введите номер'
+
+            }),
+            
+            "test_name": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Проверка базы',
 
             }),
 
